841cb79c1b6256ead7aa8582d2d622e00b1bbcb5
Ayushman <ayushsg0412@gmail.com>
2024-09-16 21:06:09 +0530

    Add signout route

--- main.go
+++ main.go
@@ -1,57 +1,62 @@
 package main
 
 import (
-    "fmt"
-    "github.com/Spartan09/lenslocked/controllers"
-    "github.com/Spartan09/lenslocked/models"
-    "github.com/Spartan09/lenslocked/templates"
-    "github.com/Spartan09/lenslocked/views"
-    "github.com/go-chi/chi/v5"
-    "github.com/gorilla/csrf"
-    "net/http"
+	"fmt"
+	"github.com/Spartan09/lenslocked/controllers"
+	"github.com/Spartan09/lenslocked/models"
+	"github.com/Spartan09/lenslocked/templates"
+	"github.com/Spartan09/lenslocked/views"
+	"github.com/go-chi/chi/v5"
+	"github.com/gorilla/csrf"
+	"net/http"
 )
 
 func main() {
-    r := chi.NewRouter()
+	r := chi.NewRouter()
 
-    r.Get("/", controllers.StaticHandler(views.Must(
-        views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
-    r.Get("/contact", controllers.StaticHandler(views.Must(
-        views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
-    r.Get("/faq", controllers.FAQ(
-        views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
+	r.Get("/", controllers.StaticHandler(views.Must(
+		views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(
+		views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
+	r.Get("/faq", controllers.FAQ(
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
-    cfg := models.DefaultPostgresConfig()
-    db, err := models.Open(cfg)
-    if err != nil {
-        panic(err)
-    }
+	cfg := models.DefaultPostgresConfig()
+	db, err := models.Open(cfg)
+	if err != nil {
+		panic(err)
+	}
 
-    defer db.Close()
-    userService := models.UserService{
-        DB: db,
-    }
-    usersC := controllers.Users{
-        UserService: &userService,
-    }
-    usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
-    usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.gohtml", "tailwind.gohtml"))
-    r.Get("/users/new", usersC.New)
-    r.Post("/users", usersC.Create)
-    r.Get("/signin", usersC.SignIn)
-    r.Post("/signin", usersC.ProcessSignIn)
+	defer db.Close()
+	userService := models.UserService{
+		DB: db,
+	}
+	sessionService := models.SessionService{
+		DB: db,
+	}
+	usersC := controllers.Users{
+		UserService:    &userService,
+		SessionService: &sessionService,
+	}
+	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
+	usersC.Templates.SignIn = views.Must(views.ParseFS(templates.FS, "signin.gohtml", "tailwind.gohtml"))
+	r.Get("/users/new", usersC.New)
+	r.Post("/users", usersC.Create)
+	r.Get("/signin", usersC.SignIn)
+	r.Post("/signin", usersC.ProcessSignIn)
+	r.Post("/signout", usersC.ProcessSignOut)
 
-    r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-        http.Error(w, "Page not found", http.StatusNotFound)
-    })
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 
-    r.Get("/users/me", usersC.CurrentUser)
+	r.Get("/users/me", usersC.CurrentUser)
 
 	csrfKey := "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
 	csrfMw := csrf.Protect(
 		[]byte(csrfKey),
 		csrf.Secure(false))
 
-    fmt.Println("Starting the server on :3000...")
-    http.ListenAndServe(":3000", csrfMw(r))
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", csrfMw(r))
 }
