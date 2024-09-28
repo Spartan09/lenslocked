d21a680d84d8b9d5e81a21a960ef97dd1eef5be7
Ayushman <ayushsg0412@gmail.com>
2024-09-28 11:05:04 +0530

    Update error message for resource not found
    Details of the error message were clarified for better understanding. The new message now reads "models: resource could not be found". This makes the message slightly more descriptive and clear.

--- models/errors.go
+++ models/errors.go
@@ -5,5 +5,5 @@ import "errors"
 var (
 	ErrEmailTaken      = errors.New("models: email address is already in use")
 	ErrInvalidPassword = errors.New("models: invalid password")
-	ErrNotFound        = errors.New("models: resource not found")
+	ErrNotFound        = errors.New("models: resource could not be found")
 )
