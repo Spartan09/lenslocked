b6bae1649bdc7c295d0a80114dd0228715ac04f0
Ayushman <ayushsg0412@gmail.com>
2024-09-30 01:14:28 +0530

    Refactor to use GalleryService for image fetching
    Replaced HTTP request and JSON parsing with a call to `GalleryService.Images` method. This simplifies the code structure and leverages the existing `models` package for fetching images.

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,38 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
+	"github.com/Spartan09/lenslocked/models"
 )
 
 func main() {
-	url := "https://api.thecatapi.com/v1/images/search?limit=10"
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Printf("Error making request: %v", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("Error reading response body: %v\n", err)
-		return
-	}
-
-	var catImagesJson []map[string]interface{}
-	if err := json.Unmarshal(body, &catImagesJson); err != nil {
-		fmt.Printf("Error unmarshalling response body: %v\n", err)
-		return
-	}
-
-	var catImages []string
-	for img := range catImagesJson {
-		catImages = append(catImages, catImagesJson[img]["url"].(string))
-	}
-
-	fmt.Println(catImages)
-
+	gs := models.GalleryService{}
+	fmt.Println(gs.Images(2))
 }
