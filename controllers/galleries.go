741b7763a403e3c2376f2f59dc8f0a078df4d750
Ayushman <ayushsg0412@gmail.com>
2024-09-30 01:10:19 +0530

    Add image handling to gallery edit page
    Introduced a new Image type and updated the data structure to include images associated with each gallery. This change fetches images from the GalleryService and passes them to the edit template, ensuring filenames are URL-encoded.

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -5,9 +5,10 @@ import (
 	"github.com/Spartan09/lenslocked/context"
 	"github.com/Spartan09/lenslocked/errors"
 	"github.com/Spartan09/lenslocked/models"
-	"github.com/Spartan09/lenslocked/services"
 	"github.com/go-chi/chi/v5"
 	"net/http"
+	"net/url"
+	"path/filepath"
 	"strconv"
 )
 
@@ -51,12 +52,30 @@ func (g Galleries) Edit(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		return
 	}
-	data := struct {
-		ID    int
-		Title string
-	}{
-		ID:    gallery.ID,
-		Title: gallery.Title,
+	type Image struct {
+		GalleryID       int
+		Filename        string
+		FilenameEscaped string
+	}
+	var data struct {
+		ID     int
+		Title  string
+		Images []Image
+	}
+	data.ID = gallery.ID
+	data.Title = gallery.Title
+	images, err := g.GalleryService.Images(gallery.ID)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
+	for _, image := range images {
+		data.Images = append(data.Images, Image{
+			GalleryID:       image.GalleryID,
+			Filename:        image.Filename,
+			FilenameEscaped: url.PathEscape(image.Filename),
+		})
 	}
 	g.Templates.Edit.Execute(w, r, data)
 }
