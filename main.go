e5419c72112e53043b4cc45045be49ac8fad7e7b
Ayushman <ayushsg0412@gmail.com>
2024-09-29 19:59:08 +0530

    Add delete route for galleries
    This commit introduces a new endpoint for deleting galleries. The new route listens for POST requests at "/{id}/delete" and calls the Delete method in galleriesC. This allows users to remove galleries via the API.

--- main.go
+++ main.go
@@ -168,6 +168,7 @@ func main() {
 			r.Post("/", galleriesC.Create)
 			r.Get("/{id}/edit", galleriesC.Edit)
 			r.Post("/{id}", galleriesC.Update)
+			r.Post("/{id}/delete", galleriesC.Delete)
 		})
 	})
 
