be4a8f86a39162fc8a270ac6bf14a6cfa24d8f2b
Ayushman <ayushsg0412@gmail.com>
2024-09-30 18:46:04 +0530

    Move server code from main.go to cmd/server/server.go
    The server initialization and configuration loading were moved to a dedicated directory and file for better project organization. This change improves modularity and aligns with Go's convention for larger projects.

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -33,20 +33,33 @@ func loadEnvConfig() (config, error) {
 	if err != nil {
 		return cfg, err
 	}
-	cfg.PSQL = models.DefaultPostgresConfig()
+	cfg.PSQL = models.PostgresConfig{
+		Host:     os.Getenv("PSQL_HOST"),
+		Port:     os.Getenv("PSQL_PORT"),
+		User:     os.Getenv("PSQL_USER"),
+		Password: os.Getenv("PSQL_PASSWORD"),
+		Database: os.Getenv("PSQL_DATABASE"),
+		SSLMode:  os.Getenv("PSQL_SSL_MODE"),
+	}
+	if cfg.PSQL.Host == "" && cfg.PSQL.Port == "" {
+		return cfg, fmt.Errorf("no PSQL Config provided")
+	}
+
 	cfg.SMTP.Host = os.Getenv("SMTP_HOST")
 	portStr := os.Getenv("SMTP_PORT")
 	cfg.SMTP.Port, err = strconv.Atoi(portStr)
 	if err != nil {
 		return cfg, err
 	}
+
 	cfg.SMTP.Username = os.Getenv("SMTP_USERNAME")
 	cfg.SMTP.Password = os.Getenv("SMTP_PASSWORD")
-	// TODO: Read the CSRF values from an ENV variable
-	cfg.CSRF.Key = "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
-	cfg.CSRF.Secure = false
-	// TODO: Read the server values from an ENV variable
-	cfg.Server.Address = ":3000"
+
+	cfg.CSRF.Key = os.Getenv("CSRF_KEY")
+	cfg.CSRF.Secure = os.Getenv("CSRF_SECURE") == "true"
+
+	cfg.Server.Address = os.Getenv("SERVER_ADDRESS")
+
 	return cfg, nil
 }
 
